c1f92044b2f17a084dfc9f1f307a14660cffb966
Samuu23 <samuelarzelan18@gmail.com>
2022-09-09 00:41:57 -0300

    funcion save

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const fs= require('fs')
 
 class Contenedor {
-    constructor(rutaArchivo){
-        this.rutaArchivo = rutaArchivo
+    constructor(fileRute){
+        this.fileRute = fileRute
     }
 
-    save (obj){
+    async #leerUnArchivo(){
+        try {
+            const data= await fs.promises.readFile(this.fileRute, 'utf-8')
+            const parseData= JSON.parse(data)
+            return parseData
 
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async save (obj){
+       const fileContent = await this.#leerUnArchivo()
+        if(fileContent.length!==0){
+            console.log(fileContent)
+            await fs.promises.writeFile(this.fileRute, JSON.stringify([...fileContent, {...obj, id: fileContent[fileContent.length-1].id + 1}], null, 2), 'utf-8')
+        }else{
+            await fs.promises.writeFile(this.fileRute, JSON.stringify([{...obj, id: 1}]), 'utf-8')
+        }
+    }
+
+    async getById(id){
+    }
+
+    async getAll(){
     }
 }
 
-const contendor= new Contenedor ('./productos.txt')
+const contenedor= new Contenedor ('./productos.txt')
+
+contenedor.save()
