77c4b1670bbdf080b58d07a1f9b366f1b7803efa
Samuu23 <samuelarzelan18@gmail.com>
2022-09-09 01:31:26 -0300

    agrego fun deleteAll y DeleteById - uso de try/Catch

--- index.js
+++ index.js
@@ -17,34 +17,77 @@ class Contenedor {
     }
 
     async save (obj){
-       const fileContent = await this.#leerUnArchivo()
-        if(fileContent.length!==0){
-            console.log(fileContent)
-            await fs.promises.writeFile(this.fileRute, JSON.stringify([...fileContent, {...obj, id: fileContent[fileContent.length-1].id + 1}], null, 2), 'utf-8')
-        }else{
-            await fs.promises.writeFile(this.fileRute, JSON.stringify([{...obj, id: 1}]), 'utf-8')
+        try {
+            const fileContent = await this.#leerUnArchivo()
+            if(fileContent.length!==0){
+                await fs.promises.writeFile(this.fileRute, JSON.stringify([...fileContent, {...obj, id: fileContent[fileContent.length-1].id + 1}], null, 2), 'utf-8')
+                
+            }else{
+                await fs.promises.writeFile(this.fileRute, JSON.stringify([{...obj, id: 1}]), 'utf-8')
+            }
+        } catch (error) {
+            console.log(error)
         }
     }
 
     async getById(idProduct){
-        const data= await this.#leerUnArchivo()
-        if(data!==undefined){
-            let productId=data.find((product)=> product.id===idProduct)
-            console.log(productId)
+        try {
+            const data= await this.#leerUnArchivo()
+            if(data!==undefined){
+                let productId=data.find((product)=> product.id===idProduct)
+                console.log(productId)
+            }
+        } catch (error) {
+            
         }
     }
 
 
     async getAll(){
-        const fileContent = await this.#leerUnArchivo()
-        if(fileContent.length!==0){
-            console.log(fileContent)
-        }else{
-            console.log('No se Encuentra Ningun producto')
+        try {
+            const fileContent = await this.#leerUnArchivo()
+            if(fileContent.length!==0){
+                console.log(fileContent)
+            }else{
+                console.log('No se Encuentra Ningun producto')
+            }
+        } catch (error) {
+            console.log(error)
         }
+    }
+
+    async deleteById(idProduct){
+        try {
+            const data = await this.#leerUnArchivo()
+            if(data!==undefined){
+                const productFilter= data.filter((product)=> product.id !== idProduct)
+                await fs.promises.writeFile(this.fileRute, JSON.stringify(productFilter, null, 4))
+                console.log(productFilter)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async deleteAll(){
+        try {
+            await fs.promises.writeFile(this.fileRute, JSON.stringify([], null, 2), 'utf-8')
+            console.log('data delete')
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 }
 
 const contenedor= new Contenedor ('./productos.txt')
 
+contenedor.save({nombre: "Zapatillas nike", precio: "20000"})
+
 contenedor.getAll()
+
+contenedor.getById(2)
+
+contenedor.deleteById(2)
+
+// contenedor.deleteAll()
